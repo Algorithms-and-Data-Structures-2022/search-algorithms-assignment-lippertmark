359e7684ae12d1390e39a8f94129f8c35edd9dc5
lippertmark <lippert.mark.d@gmail.com>
2022-05-05 23:39:22 +0500

    binary solved

--- src/binary_search_iterative.cpp
+++ src/binary_search_iterative.cpp
@@ -7,13 +7,16 @@ namespace assignment {
     // Tips:
     // 1. Заведите две переменные: (а) индекс левой границы и (б) индекс правой границы.
     int l = 0;
-    int r = data.size()-1;
+    int r = data.size();
     // 2. Поиск ведется пока индекс левой границы не превысил индекс правой.
     if (r == 0){
       return std::nullopt;
     }
-    while (l<r){
+    while (l<=r){
       int m = (l+r)/2;
+      if ((m<0) || (m>=data.size())){
+        return std::nullopt;
+      }
       if (data[m] == search_element){
         return m;
       }
