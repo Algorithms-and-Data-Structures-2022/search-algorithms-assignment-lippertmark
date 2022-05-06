126ba44787caf5dfbdfe8c910befa623c9035543
Ramil Safin <RamilSafNab1996@gmail.com>
2022-05-06 11:24:40 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,5 @@
 #include <iostream>
 
-using namespace std;
-using namespace assignment;
-
 int main(int argc, char **argv) {
-  vector <int> arr;
-  arr.push_back(1);
-  arr.push_back(4);
-  arr.push_back(5);
-  cout<< search_strategy.Search(data, search_elem)
   return 0;
 }
